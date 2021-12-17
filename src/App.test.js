bffd099ff2a4a722e89808f92d4aead40fcc933c
CostasAK <costas@kokke.eu>
2021-12-17 23:36:33 +0100

    updated trivial test

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/costas a\. kokke/i);
+  const linkElement = screen.getByText(/Made by costas a\. kokke/i);
   expect(linkElement).toBeInTheDocument();
 });
