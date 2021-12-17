35f1db2930e9ea64b883e4c5851b5b292ac942ac
CostasAK <costas@kokke.eu>
2021-12-17 23:31:35 +0100

    gh-pages workflow

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
+
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/costas a\. kokke/i);
   expect(linkElement).toBeInTheDocument();
 });
