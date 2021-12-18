76ca35ef02f5149896781a32d2923c4ae388fbbd
CostasAK <costas@kokke.eu>
2021-12-18 19:07:10 +0100

    fixed position header and footer. header transitions on scrolling. Some testing main content

--- src/App.js
+++ src/App.js
@@ -18,13 +18,34 @@ function App() {
     <div className="App">
       <header className={"header" + (scroll ? " scrolled" : "")}>
         <div className={"header_image" + (scroll ? " scrolled" : "")}>
-          <img src={avatar} alt="" />
+          <img src={avatar} alt="" className={scroll ? "scrolled" : ""} />
         </div>
-        <div className="title">Costas A. Kokke</div>
+        <div className={"title" + (scroll ? " scrolled" : "")}>
+          Costas A. Kokke
+        </div>
+        <div className="invisible"></div>
       </header>
       <main className={"main" + (scroll ? " scrolled" : "")}>
         <h1>Publications</h1>
-        <p>Stuff</p>
+        <h2>Binaural Beamforming Based on Automatic Interferer Selection</h2>
+        <h3>Abstract</h3>
+        <p>
+          Binaural cues are important for sound localization. In addition,
+          spatially separated sound sources are more intelligible than when they
+          are co-located. Binaural cue preservation in multi-microphone hearing
+          assistive devices is therefore important for the user’s listening
+          experience and safety. A number of
+          linearly-constrained-minimum-variance (LCMV) based methods exist for
+          this purpose. These are all limited in the number of sources for which
+          they can preserve the binaural cues. We propose a method of
+          automatically selecting the most important interfering sources using
+          convex optimization. The proposed method is compared, using simulation
+          experiments, to existing methods in terms of noise suppression and
+          localization errors. It improves the performance of the joint binaural
+          LCMV beam-former, by giving it more degrees of freedom for noise
+          reduction and allows a larger number of (virtual) sources present in
+          the scene.
+        </p>
         <h1>Web Apps</h1>
         <h2>FFXIV Timers</h2>
         <p>
@@ -34,6 +55,16 @@ function App() {
         <h1>Experience</h1>
         <p>Stuff</p>
         <h1>Education</h1>
+        <p>Stuff</p>
+        <p>Stuff</p>
+        <p>Stuff</p>
+        <p>Stuff</p>
+        <p>Stuff</p>
+        <p>Stuff</p>
+        <p>Stuff</p>
+        <p>Stuff</p>
+        <p>Stuff</p>
+        <p>Stuff</p>
         <p>Stuff</p>
       </main>
       <footer className="footer">
