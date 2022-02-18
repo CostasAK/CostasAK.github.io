d372ce3238b6edd2895bdf9d9f34aead1ab6acf3
CostasAK <costas@kokke.eu>
2022-02-18 18:28:23 +0100

    switched to scss and functional component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,20 +1,18 @@
-import "./style.css";
+import "./style.scss";
 
 import React from "react";
 import { SiGithub } from "react-icons/si";
 
-export class Footer extends React.Component {
-  render() {
-    return (
-      <footer className="footer">
-        <div>
-          <span>Made by Costas A. Kokke, MSc.</span>
-        </div>
-        <a href="https://github.com/CostasAK/costasak.github.io">
-          <SiGithub />
-          <span>Source</span>
-        </a>
-      </footer>
-    );
-  }
+export function Footer() {
+  return (
+    <footer className="footer">
+      <div>
+        <span>Made by Costas A. Kokke, MSc.</span>
+      </div>
+      <a href="https://github.com/CostasAK/costasak.github.io">
+        <SiGithub />
+        <span>Source</span>
+      </a>
+    </footer>
+  );
 }
