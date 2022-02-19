83af9c3b1a06462981db80722dd8f967487022b8
CostasAK <costas@kokke.eu>
2022-02-19 13:14:38 +0100

    switch to sass and new bio component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,10 @@ import "./App.scss";
 import { Bio } from "./components/Bio";
 import { Footer } from "./components/Footer";
 
-// import { Main } from "./components/Main";
-
 function App() {
   return (
     <div className="App">
       <Bio />
-      {/* <Main /> */}
       <Footer />
     </div>
   );
