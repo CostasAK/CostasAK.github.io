e586c3c218ed16827e601590a299fa5750d99c7c
CostasAK <costas@kokke.eu>
2022-06-20 10:34:44 +0200

    added responsive padding, eventlistener for accurate viewheight, centered my name and added avatar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import { Center, Heading, VStack } from "@chakra-ui/react";
+import { Center, Heading, Image, VStack } from "@chakra-ui/react";
 
 import { Bio } from "sections/Bio";
 import { Socials } from "sections/Socials";
+import avatar from "./assets/avatar_140.jpg";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const updateInnerHeight = () =>
+      document.documentElement.style.setProperty(
+        "--vh",
+        `${window.innerHeight}px`
+      );
+
+    updateInnerHeight();
+
+    window.addEventListener("resize", updateInnerHeight);
+
+    return () => window.removeEventListener("resize", updateInnerHeight);
+  }, []);
+
   return (
-    <Center h="100vh">
+    <Center minH="var(--vh)" padding={["1rem", "2rem", "4rem"]}>
       <VStack spacing="2.5rem">
-        <Heading as="h1" variant="outline">
+        <Image src={avatar} boxSize="280px" borderRadius="full" />
+        <Heading as="h1" variant="outline" textAlign="center">
           Costas A. Kokke
         </Heading>
         <Bio />
