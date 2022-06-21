fd7892dffe383c4c64addef850afd7a07eb7a606
CostasAK <costas@kokke.eu>
2022-06-21 08:17:33 +0200

    shape-rendering -> shapeRendering

--- src/assets/github.jsx
+++ src/assets/github.jsx
@@ -1,7 +1,7 @@
 export const GithubIcon = (props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    shape-rendering="crispEdges"
+    shapeRendering="crispEdges"
     viewBox="0 -0.5 16 16"
     width="16"
     height="16"
