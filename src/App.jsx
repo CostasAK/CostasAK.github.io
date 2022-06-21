0be992964bdf934ccdea08e4a349dddc1a114bd1
CostasAK <costas@kokke.eu>
2022-06-21 16:15:09 +0200

    implemented event listener with usemount hook

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,19 @@ import { Center, Container, Heading, Image, VStack } from "@chakra-ui/react";
 import { Bio } from "sections/Bio";
 import { Socials } from "sections/Socials";
 import avatar from "./assets/avatar_140.jpg";
-import { useEffect } from "react";
+import { useMount } from "hooks/useMount";
+
+const updateInnerHeight = () =>
+  document.documentElement.style.setProperty("--vh", `${window.innerHeight}px`);
 
 function App() {
-  useEffect(() => {
-    const updateInnerHeight = () =>
-      document.documentElement.style.setProperty(
-        "--vh",
-        `${window.innerHeight}px`
-      );
-
+  useMount(() => {
     updateInnerHeight();
 
     window.addEventListener("resize", updateInnerHeight);
 
     return () => window.removeEventListener("resize", updateInnerHeight);
-  }, []);
+  });
 
   return (
     <Center minH="var(--vh)" padding={["1rem", "2rem", "4rem"]}>
