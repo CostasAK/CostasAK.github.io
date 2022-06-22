add2de87758d21b336d9f211b26f8df042f22564
CostasAK <costas@kokke.eu>
2022-06-22 12:36:50 +0200

    colorful linkedin and kofi icons

--- src/theme/components/Link.js
+++ src/theme/components/Link.js
@@ -1,25 +1,19 @@
 export const Link = {
   baseStyle: ({ colorMode }) => ({
     color: colorMode === "dark" ? "blue.400" : "blue.500",
+    _hover: {
+      filter: "opacity(0.8)",
+    },
   }),
   variants: {
     linkedin: ({ colorMode }) => ({
       color: "#2577b9",
-      _hover: {
-        filter: colorMode === "dark" ? "brightness(1.25)" : "brightness(0.75)",
-      },
     }),
     github: ({ colorMode }) => ({
       color: colorMode === "dark" ? "#f3f1f1" : "#2a2d2f",
-      _hover: {
-        filter: colorMode === "dark" ? "brightness(1.25)" : "brightness(0.75)",
-      },
     }),
     kofi: ({ colorMode }) => ({
-      color: "#ff5e5b",
-      _hover: {
-        filter: colorMode === "dark" ? "brightness(1.25)" : "brightness(0.75)",
-      },
+      color: "#29abe0",
     }),
   },
 };
