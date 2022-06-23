1acc1047642375bc8fb0b05122c43681a162accb
CostasAK <costas@kokke.eu>
2022-06-23 14:59:04 +0200

    added tooltip to color mode switch

--- src/components/ColorModeSwitch.jsx
+++ src/components/ColorModeSwitch.jsx
@@ -1,4 +1,10 @@
-import { Button, Icon, forwardRef, useColorMode } from "@chakra-ui/react";
+import {
+  Button,
+  Icon,
+  Tooltip,
+  forwardRef,
+  useColorMode,
+} from "@chakra-ui/react";
 
 import { AstralIcon } from "assets/astral";
 import { UmbralIcon } from "assets/umbral";
@@ -7,19 +13,21 @@ export const ColorModeSwitch = forwardRef((props, ref) => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <Button
-      onClick={toggleColorMode}
-      pos="fixed"
-      right="8"
-      top="6"
-      zIndex="sticky"
-      paddingInline="2px"
-    >
-      <Icon
-        as={colorMode === "light" ? UmbralIcon : AstralIcon}
-        boxSize="32px"
-        transform="translateY(-7px)"
-      />
-    </Button>
+    <Tooltip label={colorMode === "light" ? "Dark Mode" : "Light Mode"}>
+      <Button
+        onClick={toggleColorMode}
+        pos="fixed"
+        right="8"
+        top="6"
+        zIndex="sticky"
+        paddingInline="2px"
+      >
+        <Icon
+          as={colorMode === "light" ? UmbralIcon : AstralIcon}
+          boxSize="32px"
+          transform="translateY(-7px)"
+        />
+      </Button>
+    </Tooltip>
   );
 });
