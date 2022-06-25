220f4b9dda3829fefe1f2fd7b0384095d0d1f9ff
CostasAK <costas@kokke.eu>
2022-06-25 20:28:12 +0200

    implemented contents button and earlier scrollToTopButton

--- src/components/ColorModeSwitch.jsx
+++ src/components/ColorModeSwitch.jsx
@@ -14,15 +14,7 @@ export const ColorModeSwitch = forwardRef((props, ref) => {
 
   return (
     <Tooltip label={colorMode === "light" ? "Dark Mode" : "Light Mode"}>
-      <Button
-        onClick={toggleColorMode}
-        pos="fixed"
-        right="8"
-        top="6"
-        zIndex="sticky"
-        size=""
-        p="3px 4px"
-      >
+      <Button onClick={toggleColorMode} size="" p="3px 4px">
         <Icon
           as={colorMode === "light" ? UmbralIcon : AstralIcon}
           boxSize="32px"
