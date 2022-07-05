dc4b665a3d4ea5918be9efe3fcf65b635a03476d
CostasAK <costas@kokke.eu>
2022-07-05 12:50:22 +0200

    dont import react entirely, only strictmode

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import "@fontsource/press-start-2p";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 
 import App from "./App";
-import React from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import theme from "theme";
 
@@ -11,10 +11,10 @@ const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <ChakraProvider resetCSS={true} theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
-  </React.StrictMode>
+  </StrictMode>
 );
